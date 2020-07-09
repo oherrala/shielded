af21dee2ee05926f730f87b95209d946d37879a4
Ossi Herrala <oherrala@iki.fi>
2020-07-09 11:57:55 +0300

    Add more tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,5 @@
-use shielded::Shielded;
 use quickcheck::quickcheck;
+use shielded::Shielded;
 
 #[test]
 fn test_shielded_unshield() {
@@ -12,10 +12,49 @@ fn test_shielded_unshield() {
     assert_eq!(original, unshielded.as_ref());
 }
 
+#[test]
+fn test_from_vec() {
+    let buf = b"hello world".to_vec();
+
+    let original = buf.clone();
+    let mut shielded = Shielded::from(buf);
+
+    let unshielded = shielded.unshield();
+    assert_eq!(original, unshielded.as_ref());
+}
+
+#[test]
+fn test_unshielded_as_mut() {
+    let buf: Vec<u8> = b"hello".to_vec();
+    let mut shielded = Shielded::from(buf);
+
+    {
+        let mut unshielded = shielded.unshield();
+        let buf: &mut [u8] = unshielded.as_mut();
+        buf[0] = b'b';
+    }
+
+    let unshielded = shielded.unshield();
+    assert_eq!(b"bello", unshielded.as_ref());
+}
+
 quickcheck! {
     fn prop_shield_unshield(xs: Vec<u8>) -> bool {
         let original = xs.clone();
         let mut shielded = Shielded::new(xs);
+        let unshielded = shielded.unshield();
+        original == unshielded.as_ref()
+    }
+
+    fn prop_shield_unshield_shield_unshield(xs: Vec<u8>) -> bool {
+        let original = xs.clone();
+        let mut shielded = Shielded::new(xs);
+
+        {
+            let unshielded = shielded.unshield();
+            assert_eq!(original, unshielded.as_ref());
+        }
+
         let unshielded = shielded.unshield();
         original == unshielded.as_ref()
     }
